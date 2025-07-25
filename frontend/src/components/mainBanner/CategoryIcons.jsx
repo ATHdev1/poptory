5a386c2457f2ef33db790d8d2a490ab44162c0fc
Joey <maesil007@gmail.com>
2025-07-25 18:23:23 +0900

    mainbanner에 categorymap 추가함.

---
+++ frontend/src/components/mainBanner/CategoryIcons.jsx
@@ -0,0 +1,21 @@
+import { CgGames } from "react-icons/cg";
+import { GiClothes } from "react-icons/gi";
+import { TbPerfume } from "react-icons/tb";
+import { IoFastFood } from "react-icons/io5";
+import { GiBullyMinion } from "react-icons/gi";
+import { GiPaintBucket } from "react-icons/gi";
+import { MdMonitorHeart } from "react-icons/md";
+import { FaHeart } from "react-icons/fa";
+
+const categoryIcons = [
+  CgGames,
+  GiClothes,
+  TbPerfume,
+  IoFastFood,
+  GiBullyMinion,
+  GiPaintBucket,
+  MdMonitorHeart,
+  FaHeart,
+];
+
+export default categoryIcons;
--- frontend/src/components/mainBanner/MainBanner.jsx
+++ frontend/src/components/mainBanner/MainBanner.jsx
@@ -1,16 +1,27 @@
 import './MainBanner.css';
+import categoryIcons from './CategoryIcons';
 
 function MainBanner() {
-    return(
+
+    return (
         <div className='MainBanner-wrapper'>
             <div className='MainBanner-subwrapper'>
                 <div className='carousel-wrapper'>
                     <div className='carousel-display'></div>
                 </div>
-                <div className='category-map'>yeah</div>
+
+                <div className='category-map'>
+                    {categoryIcons.map(function(Icon, i) {
+                        return (
+                            <div key={i}>
+                                <Icon size={25}></Icon>
+                            </div>
+                        );
+                    })}
+                </div>
             </div>
         </div>
-    )
+    );
 }
 
 export default MainBanner;
