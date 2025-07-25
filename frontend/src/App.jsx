e65c4951f0688da8e7174e8aecddd9ddfa91c2ae
김지훈 <rlawlgns2519@gmail.com>
2025-07-25 18:42:24 +0900

    feat: 로컬 변경사항 커밋

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,23 +4,24 @@ import Home from './pages/Home/Home.jsx';
 import Navbar from './components/navbar/navbar.jsx';
 
 function App() {
-  const [msg, setMsg] = useState('Loading...')
+  const [msg, setMsg] = useState('Loading...');
 
   useEffect(() => {
-    fetch('http://127.0.0.1:5000/api/hello')
+    fetch(`${import.meta.env.VITE_API_URL}`)
       .then(res => res.json())
       .then(data => setMsg(data.message))
-      .catch(err => setMsg('Error: ' + err.message))
-  }, [])
+      .catch(err => setMsg('Error: ' + err.message));
+  }, []);
 
   return (
     <div>
-      <Navbar></Navbar>
+      <Navbar />
+      <p style={{ padding: '10px', color: 'green' }}>{msg}</p>
       <div className='page-wrapper'>
-        <Home></Home>
+        <Home />
       </div>
     </div>
   );
 }
 
-export default App
+export default App;
