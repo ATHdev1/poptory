4f89a41ec108b86e5998b1598b7b7fd5251bb2de
Joey <maesil007@gmail.com>
2025-07-29 17:20:00 +0900

    지역별 랭킹 보여주는 패널의 더미 박스 제작

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -1,10 +1,13 @@
 import './Home.css';
-import MainBanner from '../../components/mainBanner/MainBanner.jsx'
+import MainBanner from '../../components/mainBanner/MainBanner.jsx';
+import LocalRankPanel from '../../components/LocalRankPanel/LocalRank.jsx';
 
 function Home(){
     return(
         <>
             <MainBanner></MainBanner>
+            <div style={{width:'100%', height:'10px', backgroundColor:'#eeeeee'}}></div>
+            <LocalRankPanel></LocalRankPanel>
         </>
     )
 }
