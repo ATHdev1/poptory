4d822b780b3b413c63ffd30e2d467ebd477e2ff3
Joey <maesil007@gmail.com>
2025-07-30 19:03:46 +0900

    홈 화면에서 추천팝업 넣음

--- frontend/src/components/mainBanner/MainBanner.jsx
+++ frontend/src/components/mainBanner/MainBanner.jsx
@@ -2,14 +2,19 @@ import './MainBanner.css';
 import CategoryIconGrid from '../CategoryItemGrid/CategoryItemGrid';
 import { useNavigate } from 'react-router-dom';
 
+import popups from '../../data/popups.jsx';
+
 function MainBanner() {
     const navigate = useNavigate();
+    let 테스팅용으로케러셀에넣을팝업=popups[0];
 
     return (
         <div className='MainBanner-wrapper'>
             <div className='MainBanner-subwrapper'>
                 <div className='carousel-wrapper'>
-                    <div className='carousel-display'></div>
+                    <div className='carousel-display' onClick={()=>window.location.href=`/stores/${테스팅용으로케러셀에넣을팝업.id}`} style={{backgroundImage:`url(${테스팅용으로케러셀에넣을팝업.썸네일})`}}>
+                        {/* 일단 캐러셀 말고 이미지 한 장만 넣겠음 */}
+                    </div>
                 </div>
 
                 <div>
