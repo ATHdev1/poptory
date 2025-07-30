16002b2c150cacfa307fe50bf602a945e5904e68
Joey <maesil007@gmail.com>
2025-07-30 16:50:16 +0900

    DetailPage의 overview 부분을 컴포넌트로 리팩토링함

---
+++ frontend/src/components/DetailPage_popupOverview/DetailPage_popupOverview.jsx
@@ -0,0 +1,35 @@
+import './DetailPage_popupOverview.css';
+
+function DetailPage_popupOverview({ popup }) {
+    return (
+        <div className='DetailPage-popupOverview-container'>
+            <div className='DetailPage-popupOverview-wrapper'>
+                <div className='DetailPage-popupOverview-header-wrapper'>
+                    <div className='DetailPage-popupOverview-name-wrapper'>
+                        <h2>{popup.이름}</h2>
+                    </div>
+                    <div className='DetailPage-popupOverview-region-wrapper'>
+                        <h3 style={{ color: '#777777' }}>{popup.지역}</h3>
+                    </div>
+                </div>
+                <div className='DetailPage-popupOverview-contents-container'>
+                    <div className='DetailPage-popupOverview-thumbnailImg-wrapper'>
+                        <div className='DetailPage-popupOverview-thumbnailImg'></div>
+                    </div>
+                    <div className='DetailPage-popupOverview-text-container'>
+                        <div className='DetailPage-popupOverview-text-address-wrapper'>
+                            <h4 style={{ margin: '0' }}>주소</h4>
+                            <p style={{ margin: '0' }}>{popup.장소}</p>
+                        </div>
+                        <div className='DetailPage-popupOverview-text-period-wrapper'>
+                            <h4 style={{ margin: '0' }}>일정</h4>
+                            <p style={{ margin: '0' }}>{popup.일정}</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default DetailPage_popupOverview;
--- frontend/src/pages/DetailPage/DetailPage.jsx
+++ frontend/src/pages/DetailPage/DetailPage.jsx
@@ -1,6 +1,7 @@
 import { useParams } from 'react-router-dom';
 import './DetailPage.css';
 
+import DetailPage_popupOverview from '../../components/DetailPage_popupOverview/DetailPage_popupOverview.jsx';
 import popups from '../../data/popups.jsx';
 
 function DetailPage() {
@@ -10,33 +11,7 @@ function DetailPage() {
     })
     return (
         <div>
-            <div className='DetailPage-popupOverview-container'>
-                <div className='DetailPage-popupOverview-wrapper'>
-                    <div className='DetailPage-popupOverview-header-wrapper'>
-                        <div className='DetailPage-popupOverview-name-wrapper'>
-                            <h2>{popup.이름}</h2>
-                        </div>
-                        <div className='DetailPage-popupOverview-region-wrapper'>
-                            <h3 style={{ color: '#777777' }}>{popup.지역}</h3>
-                        </div>
-                    </div>
-                    <div className='DetailPage-popupOverview-contents-container'>
-                        <div className='DetailPage-popupOverview-thumbnailImg-wrapper'>
-                            <div className='DetailPage-popupOverview-thumbnailImg'></div>
-                        </div>
-                        <div className='DetailPage-popupOverview-text-container'>
-                            <div className='DetailPage-popupOverview-text-address-wrapper'>
-                                <h4 style={{ margin: '0' }}>주소</h4>
-                                <p style={{ margin: '0' }}>{popup.장소}</p>
-                            </div>
-                            <div className='DetailPage-popupOverview-text-period-wrapper'>
-                                <h4 style={{ margin: '0' }}>일정</h4>
-                                <p style={{ margin: '0' }}>{popup.일정}</p>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
+            <DetailPage_popupOverview popup={popup}></DetailPage_popupOverview>
         </div>
     )
 }
