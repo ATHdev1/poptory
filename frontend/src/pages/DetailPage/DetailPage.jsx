1ed702618e83a02e746d06e8bf765a5c1a624983
Joey <maesil007@gmail.com>
2025-07-30 17:11:54 +0900

    DetailPage 에서 상세정보 버튼 누르면 상세정보 보여주는 기능 추가

--- frontend/src/pages/DetailPage/DetailPage.jsx
+++ frontend/src/pages/DetailPage/DetailPage.jsx
@@ -3,15 +3,33 @@ import './DetailPage.css';
 
 import DetailPage_popupOverview from '../../components/DetailPage_popupOverview/DetailPage_popupOverview.jsx';
 import popups from '../../data/popups.jsx';
+import { useState } from 'react';
 
 function DetailPage() {
     const { id } = useParams();
     const [popup] = popups.filter(function (popup) {
         return popup.id == id;
     })
+
+    let [ showDetail, setShowDetail ] = useState(false)
+
     return (
         <div>
             <DetailPage_popupOverview popup={popup}></DetailPage_popupOverview>
+
+            <div className='DetailPage-detailInfo-container'>
+                <div className='DetailPage-showMore-button-wrapper'>
+                    <div className='DetailPage-showMore-button' onClick={function(){
+                        setShowDetail(!showDetail)
+                    }}>
+                        <p style={{ fontSize: '18px' }}>상세 정보</p>
+                    </div>
+                </div>
+                <div className='DetailPage-detailInfo-content-container'>
+                    {showDetail==true?<div>{popup.상세정보}</div>:null}
+                </div>
+            </div>
+
         </div>
     )
 }
