9d884a1c694e58148e42c2432da39458e7a57fab
Joey <maesil007@gmail.com>
2025-07-31 15:13:41 +0900

    더미데이터 추가

--- frontend/src/data/popups.jsx
+++ frontend/src/data/popups.jsx
@@ -9,7 +9,7 @@ const popups = [
         공지사항: "입장권 사전 구매 필수",
         상세정보: "포켓몬 팝업에 대한 상세 정보!",
 
-        썸네일:"https://blog.kakaocdn.net/dna/YRrLj/btszcQoHMXv/AAAAAAAAAAAAAAAAAAAAABwt0fdy7v2b8l1UVEAhkOENOqDtlHPFXvFsqHwzdw20/img.jpg?credential=yqXZFxpELC7KVnFOS48ylbz2pIh7yKj8&expires=1753973999&allow_ip=&allow_referer=&signature=DVO6RzwJYrTY5eNdvTH9000zgzQ%3D"
+        썸네일: "https://blog.kakaocdn.net/dna/YRrLj/btszcQoHMXv/AAAAAAAAAAAAAAAAAAAAABwt0fdy7v2b8l1UVEAhkOENOqDtlHPFXvFsqHwzdw20/img.jpg?credential=yqXZFxpELC7KVnFOS48ylbz2pIh7yKj8&expires=1753973999&allow_ip=&allow_referer=&signature=DVO6RzwJYrTY5eNdvTH9000zgzQ%3D"
     },
     {
         id: 2,
@@ -21,7 +21,7 @@ const popups = [
         공지사항: "입장권 사전 구매 필수",
         상세정보: "젤다 팝업에 대한 상세 정보!",
 
-        썸네일:"https://bananaroad.com/cdn/shop/products/pst3899_Zelda_Breath_of_the_Wild_Poster-copy.jpg?v=1629090071"
+        썸네일: "https://bananaroad.com/cdn/shop/products/pst3899_Zelda_Breath_of_the_Wild_Poster-copy.jpg?v=1629090071"
     },
     {
         id: 3,
@@ -33,7 +33,7 @@ const popups = [
         공지사항: "입장권 사전 구매 필수",
         상세정보: "마리오 팝업에 대한 상세 정보!",
 
-        썸네일:"https://www.nintendo.com/kr/event/popup2023/images/header_sp.png"
+        썸네일: "https://www.nintendo.com/kr/event/popup2023/images/header_sp.png"
     },
     {
         id: 4,
@@ -45,7 +45,7 @@ const popups = [
         공지사항: "입장권 사전 구매 필수",
         상세정보: "포켓몬 팝업에 대한 상세 정보!",
 
-        썸네일:"https://blog.kakaocdn.net/dna/YRrLj/btszcQoHMXv/AAAAAAAAAAAAAAAAAAAAABwt0fdy7v2b8l1UVEAhkOENOqDtlHPFXvFsqHwzdw20/img.jpg?credential=yqXZFxpELC7KVnFOS48ylbz2pIh7yKj8&expires=1753973999&allow_ip=&allow_referer=&signature=DVO6RzwJYrTY5eNdvTH9000zgzQ%3D"
+        썸네일: "https://blog.kakaocdn.net/dna/YRrLj/btszcQoHMXv/AAAAAAAAAAAAAAAAAAAAABwt0fdy7v2b8l1UVEAhkOENOqDtlHPFXvFsqHwzdw20/img.jpg?credential=yqXZFxpELC7KVnFOS48ylbz2pIh7yKj8&expires=1753973999&allow_ip=&allow_referer=&signature=DVO6RzwJYrTY5eNdvTH9000zgzQ%3D"
     },
     {
         id: 5,
@@ -57,7 +57,7 @@ const popups = [
         공지사항: "입장권 사전 구매 필수",
         상세정보: "젤다 팝업에 대한 상세 정보!",
 
-        썸네일:"https://bananaroad.com/cdn/shop/products/pst3899_Zelda_Breath_of_the_Wild_Poster-copy.jpg?v=1629090071"
+        썸네일: "https://bananaroad.com/cdn/shop/products/pst3899_Zelda_Breath_of_the_Wild_Poster-copy.jpg?v=1629090071"
     },
     {
         id: 6,
@@ -69,7 +69,7 @@ const popups = [
         공지사항: "입장권 사전 구매 필수",
         상세정보: "마리오 팝업에 대한 상세 정보!",
 
-        썸네일:"https://www.nintendo.com/kr/event/popup2023/images/header_sp.png"
+        썸네일: "https://www.nintendo.com/kr/event/popup2023/images/header_sp.png"
     },
     {
         id: 7,
@@ -81,7 +81,7 @@ const popups = [
         공지사항: "입장권 사전 구매 필수",
         상세정보: "암스트롱 팝업에 대한 상세 정보!",
 
-        썸네일:"https://mblogthumb-phinf.pstatic.net/MjAyMTA1MThfMTM5/MDAxNjIxMzA4NzM2MTY0.gt8bRoAG-zyOKniIjT9tn25rR9hPpUjskVWD3kg9opQg.tMwqcpWzuklW2aj8HiLJjBIznURyDbKjWXs5f8KgXV8g.PNG.abcmart78/img.png?type=w800"
+        썸네일: "https://mblogthumb-phinf.pstatic.net/MjAyMTA1MThfMTM5/MDAxNjIxMzA4NzM2MTY0.gt8bRoAG-zyOKniIjT9tn25rR9hPpUjskVWD3kg9opQg.tMwqcpWzuklW2aj8HiLJjBIznURyDbKjWXs5f8KgXV8g.PNG.abcmart78/img.png?type=w800"
     },
     {
         id: 8,
@@ -93,8 +93,20 @@ const popups = [
         공지사항: "입장권 사전 구매 필수",
         상세정보: "비틀즈 팝업에 대한 상세 정보!",
 
-        썸네일:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQiGhZChYli4w0gDwcdhJzKO-mG241ePFuxQg&s"
-    }
+        썸네일: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQiGhZChYli4w0gDwcdhJzKO-mG241ePFuxQg&s"
+    },
+    {
+        id: 9,
+        이름: "위버맨시 팝업스토어",
+        카테고리: "music",
+        지역: "강서",
+        장소: "마곡 식물원",
+        일정: "2025-08-15",
+        공지사항: "입장권 사전 구매 필수",
+        상세정보: "위버맨시 팝업에 대한 상세 정보!",
+
+        썸네일: "https://cdn.popga.co.kr/spot/2071/main/f95c5e0d-eb58-4bd2-8adb-5204f65b9c65_1741069868304.jpg"
+    },
 ]
 
 export default popups;
